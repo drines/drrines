2b3af45344f4e20ebd955d92043c2a46f06e26a6
Daniel Rines <drines@users.noreply.github.com>
2019-02-05 15:10:00 -0800

    Added postgresql connection info

--- drrines/settings.py
+++ drrines/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'drrines.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dev4drrines',
+        'USER': 'development4postgresuser',
+        'PASSWORD': '7c2zu%NOiv_RNesSOl#yoz',
+        'HOST': 'drrines-dev4-postgres.cpfummht5mh0.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
