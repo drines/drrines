a89ccb648ec8f8bef2633d0c40a0b00da69e12a6
Daniel Rines <drines@users.noreply.github.com>
2019-02-06 15:52:59 -0800

    updated dashboard templates location

--- drrines/settings.py
+++ drrines/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'drrines.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'dashboard/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
