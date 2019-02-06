c1d58e26df9ea7c1d8b0b60d35b1560a6ab57293
Daniel Rines <drines@users.noreply.github.com>
2019-02-05 16:10:44 -0800

    Update settings.py
    Added the ALLOWED_HOSTS and the appropriate templates URL.

--- drrines/settings.py
+++ drrines/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dzt_r5+u!*&^l$^tic*6)#3wzxn@=67eq*=hyhh5u^pod##=zx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dev.drrines.com', '3.17.2.85']
 
 
 # Application definition
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'drrines.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
